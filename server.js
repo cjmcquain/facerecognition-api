8f4f55079b8e0a9de1b615c4186edf763957cfaf
Charles McQuain <charlesmcquain@gmail.com>
2018-02-13 21:38:09 -0800

    Added Body-Parser

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send('This is working!');
+});
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+        res.json('Success');
+    } else {
+        res.status(400).json('Error logging in');
+    }
+});
 
 app.listen(3000, () => {
     console.log('Server listening on Port 3000');
