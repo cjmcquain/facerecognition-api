d8f3deaeb32e70cf1cde9dd8d2935a53d77ad76c
Charles McQuain <charlesmcquain@gmail.com>
2018-02-15 22:31:41 -0800

    Initialized connection to front-end of app

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
     users: [
@@ -51,6 +54,38 @@ app.post('/register', (req, res) => {
     })
     res.json(database.users[database.users.length - 1]);
 });
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    });
+    if (!found) {
+        res.status(404).json('Not Found');
+    }
+});
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    });
+    if (!found) {
+        res.status(404).json('Not Found');
+    }
+});
+
+
+
 
 app.listen(3000, () => {
     console.log('Server listening on Port 3000');
